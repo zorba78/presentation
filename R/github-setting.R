15c7b48266cad8f88fe44c19f3b6251190e11586
zorba78 <bonchoku78@gmail.com>
2021-02-18 19:04:27 +0900

    Initial commit

--- R/github-setting.R
+++ R/github-setting.R
@@ -15,9 +15,10 @@ remotes::install_github("ropensci/git2r")
 library(usethis)
 use_git_config(user.name = "zorba78", user.email = "bonchoku78@gmail.com")
 git_sitrep()
-# create_github_token()
+create_github_token()
 #token:  472404eb3e4122feab10e316bba0ce54d635db10
 gitcreds::gitcreds_set()
+
 usethis::use_git()
-usethis::use_git_remote("origin", url = NULL, overwrite = TRUE)
+# usethis::use_git_remote("origin", url = NULL, overwrite = TRUE)
 usethis::use_github()
