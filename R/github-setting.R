6a97a06e7962494551686fef9624eed57152c29d
zorba78 <bonchoku78@gmail.com>
2021-02-18 17:35:45 +0900

    Initial commit

---
+++ R/github-setting.R
@@ -0,0 +1,20 @@
+library(here)
+library(glue)
+library(rrtools)
+# remotes::install_github("benmarwick/rrtools")
+
+# create_compendium("~/R-project/presentation")
+# usethis::use_git()
+
+
+remotes::install_github("ropensci/git2r")
+
+#---------------------------------------------------------#
+# GITHUB setting
+#---------------------------------------------------------#
+library(usethis)
+use_git_config(user.name = "zorba78", user.email = "bonchoku78@gmail.com")
+git_sitrep()
+create_github_token()
+#token:  472404eb3e4122feab10e316bba0ce54d635db10
+usethis::use_github()
